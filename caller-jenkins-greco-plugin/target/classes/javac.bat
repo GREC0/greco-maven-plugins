cc0c4ba4005025f88f79096bf82f07e2ff0d989c
GREC0 <goyo.developer@gmail.com>
2017-04-06 20:26:21 +0200

    Delete javac.bat

--- caller-jenkins-greco-plugin/target/classes/javac.bat
+++
@@ -1,1 +0,0 @@
-cmd.exe /X /C ""C:\Program Files\Java\jdk1.8.0_112\jre\..\bin\javac.exe" @C:/Users/GOYO/workspace/caller-jenkins-greco-plugin/target/classes/org.codehaus.plexus.compiler.javac.JavacCompiler6739153037258518104arguments -J-Xmx1024m"
